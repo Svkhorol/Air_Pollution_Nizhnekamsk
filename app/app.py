12152ed7aa4816114c204d5902f23b49b5d9ffd1
Svetlana <anonda@mail.ru>
2023-12-02 19:43:50 +0300

    feat: add uploader in flask

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
+import os
 import pickle
 
-from flask import Flask, render_template, request
-#from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, flash
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__, template_folder='templates')
-#bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
+
+ALLOWED_EXTENSIONS = {'xlsx'}
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -19,7 +27,23 @@ def main():
 
     # Обработка POST-запроса
     if request.method == 'POST':
-        pass
+
+        files = request.files.getlist('file')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = file.filename.replace(' ', '_')
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+
+        # Обработка файлов
+
+        # Удаление файлов
+        for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+            if filename != '.gitkeep':
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                os.remove(file_path)
+
+        return render_template('map_SO2.html')
 
 
 if __name__ == '__main__':
